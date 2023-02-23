2a88cb7fdc8ff7252d6f1298d2056d442bb61dab
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-02-23 14:12:33 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -115,6 +115,7 @@ sudo snap install slack
 sudo snap install chromium
 sudo snap install terraform --classic
 sudo snap install dotnet-sdk --classic
+sudo snap install notion-snap
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
